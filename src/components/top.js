b4dfd75a11684d6789ce934cb6a99292e62f5ace
harshhx <hg070401@gmail.com>
2021-10-24 02:29:06 +0530

    slide

--- src/components/top.js
+++ src/components/top.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Slide } from "@mui/material";
 import "./top.css";
 function Top() {
   return (
     <div className="top">
       <div className="text">I'm Harsh Gupta</div>
+      <Slide direction="up" in={true} mountOnEnter unmountOnExit style={{ transformOrigin: '0 0 0' }}
+    {...(true ? { timeout: 2000 } : {})}>
       <div className="about">
         I am a Bachelor of Engineering Student in Information Technology from
         MAIT Delhi.
       </div>
+      </Slide>
     </div>
   );
 }
