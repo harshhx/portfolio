45040b7ba9c27aa17e9362c8b540ad11ead19762
harshhx <hg070401@gmail.com>
2021-10-24 19:32:53 +0530

    confetti added but not working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/header';
 import Top from './components/top';
+import Confetti from './components/confetti';
 function App() {
   return (
     <div className="App">
+      {/* <Confetti /> */}
       {/* <Header className="App-header" /> */}
       <Top />
     </div>
