fbf3c2b3227b18a69bf9405358835c088b3cd3c5
harshhx <hg070401@gmail.com>
2021-11-17 00:00:46 +0530

    navbar fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavItem } from "react-bootstrap";
 import "./navbar.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Skills from "./skills";
@@ -9,19 +9,32 @@ function NavbarT() {
       <div>
         <Navbar bg="dark" variant="dark" fixed="top">
           <Container>
-            <Navbar.Brand as={Link} to="/">
-              Harsh Gupta
-            </Navbar.Brand>
+            <NavItem>
+              <Navbar.Brand as={Link} to="/">
+                Harsh Gupta
+              </Navbar.Brand>
+            </NavItem>
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/skills">
-                Skills
-              </Nav.Link>
-              <Nav.Link as={Link} to="/work-experience">
-                Work Experience
-              </Nav.Link>
-              <Nav.Link as={Link} to="/education">
-                Education
-              </Nav.Link>
+              <NavItem>
+                <Nav.Link as={Link} to="/skills">
+                  Skills
+                </Nav.Link>
+              </NavItem>
+              <NavItem>
+                <Nav.Link as={Link} to="/work-experience">
+                  Work Experience
+                </Nav.Link>
+              </NavItem>
+              <NavItem>
+                <Nav.Link as={Link} to="/education">
+                  Education
+                </Nav.Link>
+              </NavItem>
+              <NavItem>
+                <Nav.Link as={Link} to="/projects">
+                  Projects
+                </Nav.Link>
+              </NavItem>
             </Nav>
           </Container>
         </Navbar>
