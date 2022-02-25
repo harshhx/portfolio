36fc917f4c6643c6a66efa4c039a03929372bbea
harshhx <hg070401@gmail.com>
2022-02-26 00:20:04 +0530

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/Harsh Gupta Blob.png'
+import HeaderSocials from "./HeaderSocials";
 
 const Header =() =>{
     return(
-        <div>Header</div>
+        <header>
+            <div className="container header_container">
+                <h5>
+                    Hello I'm
+                </h5>
+                <h1>Harsh Gupta</h1>
+                <h5 className="text-light">
+                    Software Engineering Student
+                </h5>
+                <CTA />
+                <HeaderSocials />
+
+                <div className='me'>
+                    <img src={ME} alt="me" />
+                </div>
+
+                <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+            </div>
+        </header>
     );
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+const HeaderSocials=()=>{
+    return (
+        <div className='header_socials'>
+            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
+            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
+            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
+        </div>
+    );
+}
+
+export default HeaderSocials;
