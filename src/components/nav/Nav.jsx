2f85c46a8373dc52bb3d10ca9d2671d6dc45986d
harshhx <hg070401@gmail.com>
2022-02-26 00:41:17 +0530

    nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './nav.css'
+import {AiOutlineHome} from "react-icons/all";
+import {useState} from 'react'
+
 const Nav =() =>{
+    const [activeNav, setActiveNav] = useState('#')
     return(
-        <div>Nav</div>
+        <nav>
+            <a href="#" onClick={()=>setActiveNav('#')} className={activeNav === "#"? 'active': ''}><AiOutlineHome/></a>
+            <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav === "#about"? 'active': ''}><AiOutlineHome/></a>
+            <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav === "#experience"? 'active': ''}><AiOutlineHome/></a>
+            {/*<a href="#services"><AiOutlineHome/></a>*/}
+            <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav === "#contact"? 'active': ''}><AiOutlineHome/></a>
+        </nav>
     );
 }
 
