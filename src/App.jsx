85ec0549ddc429a358e9399069c7d49c31add26f
harshhx <hg070401@gmail.com>
2022-02-25 23:26:13 +0530

    global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 
 const App =() =>{
     return(
-        <div>APP</div>
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Contact />
+        </>
     );
 }
 
