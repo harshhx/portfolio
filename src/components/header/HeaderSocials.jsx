934d71eed0582bb41014762e2775264a4e0cda4b
harshhx <hg070401@gmail.com>
2022-03-01 19:15:34 +0530

    social Icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
+import {SiLeetcode} from "react-icons/all";
+
 const HeaderSocials=()=>{
     return (
         <div className='header_socials'>
-            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
-            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
-            <a href="https://linkedin.in/harshhx" target='_blank'><BsLinkedin /></a>
+            <a href="https://www.linkedin.com/in/harshhx/" target='_blank'><BsLinkedin /></a>
+            <a href="https://leetcode.com/harshhx/" target='_blank'><SiLeetcode /></a>
+            <a href="https://github.com/harshhx" target='_blank'><BsGithub /></a>
         </div>
     );
 }
