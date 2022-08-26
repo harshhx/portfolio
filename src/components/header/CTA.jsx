3a534c33e25de85aaefe33f94a0ec6d63077f0bf
harshhx <hg070401@gmail.com>
2022-08-26 23:05:32 +0530

    fixes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import {BsDownload} from "react-icons/all";
 const CTA = () => {
     return (
         <div className='cta'>
-            <a href={CV} download className='btn btn-primary'> Download CV  <BsDownload style={{marginLeft:"0.5rem"}}/></a>
+            <a href={CV} download="Harsh_Gupta_Resume.pdf" className='btn btn-primary'> Download CV  <BsDownload style={{marginLeft:"0.5rem"}}/></a>
             <a href="#contact" className='btn'>Lets Talk</a>
         </div>
     );
